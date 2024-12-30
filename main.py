c02d683bbcc76c04266927da642d4c721f49b8e2
varunpenumudi <penumudivarun@gmail.com>
2024-12-30 20:12:21 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import google.generativeai as genai
+
+
+# ----------------------------
+# Configure Gemini Model
+# ----------------------------
+
+system_role = """
+You are a good and nice llm who answers the user queries in just one to one and half pargraphs. 
+The responses should be simple but intuitive answers should be given.
+"""
+
+GEMINI_API_KEY = "AIzaSyAvabwmok5onFLXJHRj9TeBovBjMU-HYlQ"
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel(
+    model_name="gemini-2.0-flash-exp",
+    system_instruction=system_role,
+)
+
+# Chat sesssion to store the ai messages
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
+# Simple function to get response message
+def get_resp(prompt: str) -> str:
+    response = chat_session.send_message(prompt)
+    return response.text
+
+
+
+# ----------------------------
+# STREAMLIT APP
+# ----------------------------
+
+st.title("🤖 AI Assistant")
+st.write("")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+# Display chat messages from history on app rerun
+for msg in st.session_state.messages:
+    with st.chat_message(msg['role']):
+        st.markdown(msg["content"])
+
+
+prompt = st.chat_input("What is up?")
+
+if prompt:
+    # Display user message in chat container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    # Add user message to chat history
+    st.session_state.messages.append({"role":"user", "content": prompt})
+
+    # Show response to user message 
+    resp = get_resp(prompt)
+    with st.chat_message("assistant"):
+        st.markdown(resp)
+
+    # Add also the response to history
+    st.session_state.messages.append({"role":"assistant", "content":resp})
